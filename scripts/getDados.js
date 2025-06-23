68667b45211b0a89f06227461b18de4651732896
Andre Dantas <andremoreirati@gmail.com>
2025-06-23 20:59:48 -0300

    corrigindo endereço API local

--- scripts/getDados.js
+++ scripts/getDados.js
@@ -1,6 +1,6 @@
 async function getDados() {
   try {
-    const response = await fetchfetch('https://andredantasti.github.io/desafio-time-tracking/data.json');
+    const response = await fetch('https://andredantasti.github.io/desafio-time-tracking/data.json');
     const raw = await response.json();
     const data = Array.isArray(raw) ? raw : Object.values(raw);
     return data;
